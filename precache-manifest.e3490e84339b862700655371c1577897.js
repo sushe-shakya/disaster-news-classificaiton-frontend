c4ae73b29d546b9df834365d853fd02d5c095c59
Sushil Shakya <sushil.shakya@fusemachines.com>
2019-09-25 19:36:46 +0545

    Updates

---
+++ precache-manifest.e3490e84339b862700655371c1577897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e02b3aa7ec8542a97abb0e6e298c1e1",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/index.html"
+  },
+  {
+    "revision": "f24b4a95c8676a401d40",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "67afdaa8342d0ab765c3",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/static/css/main.19152d21.chunk.css"
+  },
+  {
+    "revision": "f24b4a95c8676a401d40",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/static/js/2.8fe17a55.chunk.js"
+  },
+  {
+    "revision": "67afdaa8342d0ab765c3",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/static/js/main.e689bb9d.chunk.js"
+  },
+  {
+    "revision": "ba02e7a9bee89c21e715",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/static/js/runtime~main.3d411ee5.js"
+  },
+  {
+    "revision": "1d8592b1b6c21d09106fdf81d197bfa1",
+    "url": "/sushe-shakya/disaster-news-classification-frontend/static/media/bg-login.1d8592b1.jpg"
+  }
+]);
