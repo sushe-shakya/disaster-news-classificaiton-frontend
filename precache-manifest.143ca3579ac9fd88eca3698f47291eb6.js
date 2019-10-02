810e00527c8623543d62b1051a8beb9df2193ea2
Sushil Shakya <sushil.shakya@fusemachines.com>
2019-10-02 12:53:18 +0545

    Updates

---
+++ precache-manifest.143ca3579ac9fd88eca3698f47291eb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18aef2805cb04997bb977d462f092e1",
+    "url": "/disaster-news-classification-frontend/index.html"
+  },
+  {
+    "revision": "f24b4a95c8676a401d40",
+    "url": "/disaster-news-classification-frontend/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "371332ae2ed0b7927ccd",
+    "url": "/disaster-news-classification-frontend/static/css/main.de3528b3.chunk.css"
+  },
+  {
+    "revision": "f24b4a95c8676a401d40",
+    "url": "/disaster-news-classification-frontend/static/js/2.8fe17a55.chunk.js"
+  },
+  {
+    "revision": "371332ae2ed0b7927ccd",
+    "url": "/disaster-news-classification-frontend/static/js/main.cf4fee98.chunk.js"
+  },
+  {
+    "revision": "ad3846be44d83415dc6a",
+    "url": "/disaster-news-classification-frontend/static/js/runtime~main.fe14def7.js"
+  },
+  {
+    "revision": "1d8592b1b6c21d09106fdf81d197bfa1",
+    "url": "/disaster-news-classification-frontend/static/media/bg-login.1d8592b1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sushe-shakya/disaster-news-classification-frontend/precache-manifest.89d00a25bb62b81701a9ae75e609937d.js"
+  "/disaster-news-classification-frontend/precache-manifest.143ca3579ac9fd88eca3698f47291eb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sushe-shakya/disaster-news-classification-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/disaster-news-classification-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
